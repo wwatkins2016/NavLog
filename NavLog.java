a542ab0335733867b72cf510289e964ef95d8b8d
wwatkins2016 <wwatkins2016@gmail.com>
2016-03-11 09:42:25 -0600

    Added files via upload

--- NavLog.java
+++ NavLog.java
@@ -2,23 +2,6 @@ package navlog;
 
 public class NavLog
 {	
-	private double computeMiles(int Ndeg, int Nmin, int Nsec, 
-			int Wdeg, int Wmin, int Wsec)
-	{
-		
-	}
-	private int computeWindCorrection(int dir, int vel, int thdg)
-	{
-		
-	}
-	private int computeVariance(int whdg, int magvar)
-	{
-		
-	}
-	private double computeTime()
-	{
-		
-	}
 	public static void main(String[] args)
 	{
 		
